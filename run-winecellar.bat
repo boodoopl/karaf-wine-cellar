06c14a1695688ecfffbc1feda786a999c60878ea
Maciej Mraczek <maciej.mraczek@gmail.com>
2014-12-05 22:59:10 +0100

    Extend run script

--- run-winecellar.bat
+++ run-winecellar.bat
@@ -1,3 +1,3 @@
-SET KARAF_DEBUG = true
+rmdir assemblies\winecellar-distribution\target\assembly\system\org\karaf /s /q
 cd assemblies\winecellar-distribution\target\assembly\bin
-karaf.bat
+karaf.bat clean debug
